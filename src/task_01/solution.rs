99d6f15aa38d102875fd241b13d3144ce776bab4
mbrizic <brizic.mario@gmail.com>
2022-12-01 20:52:25 +0100

    Solved day 1

--- src/task_01/solution.rs
+++ src/task_01/solution.rs
@@ -1,4 +1,4 @@
-use crate::common::{benchmark};
+use crate::common::benchmark;
 
 pub fn run() {
     benchmark("01.1", &solve_first_part);
@@ -6,46 +6,70 @@ pub fn run() {
 }
 
 fn solve_first_part() -> i64 {
-    let values = include_str!("./input.txt").lines()
-            .map(|a| a.parse::<i32>().unwrap())
-            .collect::<Vec<i32>>();
+    let values = include_str!("./input.txt").lines().collect::<Vec<&str>>();
 
-    let mut result = 0;
+    let mut max = 0;
+    let mut acc = 0;
 
-    for index in 1..values.len() {
-        if values[index] > values[index-1] {
-            result = result + 1;
+    values.iter().for_each(|value| {
+        if value.len() == 0 {
+            if acc > max {
+                max = acc;
+            }
+            acc = 0;
+            return;
         }
-    }
 
-    assert_eq!(result, 1154);
+        let calories = value.parse::<i32>().unwrap();
 
-    return result;
+        acc += calories;
+    });
+
+    assert_eq!(max, 69626);
+
+    return max as i64;
 }
 
 fn solve_second_part() -> i64 {
+    let values = include_str!("./input.txt").lines().collect::<Vec<&str>>();
 
-    let values = include_str!("./input.txt").lines()
-            .map(|a| a.parse::<i32>().unwrap())
-            .collect::<Vec<i32>>();
+    let mut sums: Vec<i32> = vec!(0, 0, 0);
 
-    // Functional approach, equally performant
-    // let result = values
-    //         .windows(4)
-    //         .filter(|x| x[3] > x[0])
-    //         .count();
+    let mut acc = 0;
 
-    let mut result = 0;
-
-    for i in 1..values.len()-2 {
-
-        if values[i + 2] > values[i - 1] {
-            result = result + 1;
+    values.iter().for_each(|value| {
+        if value.len() == 0 {
+            enter_value_if_bigger(&mut sums, acc, 0);
+            acc = 0;
+            return;
         }
 
+        let calories = value.parse::<i32>().unwrap();
+
+        acc += calories;
+    });
+
+    let max = sums[0] + sums[1] + sums[2];
+
+    assert_eq!(max, 206780);
+
+    return max as i64;
+}
+
+
+fn enter_value_if_bigger(sums: &mut Vec<i32>, new_value: i32, index_to_check: usize) {
+    if index_to_check > 2 {
+        return ();
     }
 
-    assert_eq!(result, 1127);
+    let current_value = sums[index_to_check];
 
-    return result;
+    if new_value > current_value {
+        sums[index_to_check] = new_value;
+        enter_value_if_bigger(sums, current_value, index_to_check + 1);
+    } else {
+        enter_value_if_bigger(sums, new_value, index_to_check + 1);
+    }
+
+    return ();
 }

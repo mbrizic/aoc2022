b946cd478a6378ab27e2c5407d0fb3dc7efa7ce9
mbrizic <brizic.mario@gmail.com>
2022-12-02 13:38:39 +0100

    Chnage the code to use precalculated hashmap values for faster execution

--- src/task_02/solution.rs
+++ src/task_02/solution.rs
@@ -7,45 +7,35 @@ pub fn run() {
     benchmark("02.2", &solve_second_part);
 }
 
+const LOSS: i32 = 0;
 const DRAW: i32 = 3;
 const WIN: i32 = 6;
+
+const ROCK: i32 = 1;
+const PAPER: i32 = 2;
+const SCISSORS: i32 = 3;
 
 fn solve_first_part() -> i64 {
     let lines = include_str!("./input.txt")
         .lines()
-        .map(|value| value.split(' ').collect::<Vec<&str>>())
-        .collect::<Vec<Vec<&str>>>();
+        .collect::<Vec<&str>>();
 
     let mut total_score = 0;
 
-    let points: HashMap<&str, i32> = HashMap::from([
-        ("A", 1), // ROCK
-        ("B", 2), // PAPER
-        ("C", 3), // SCISSORS
-        ("X", 1), // ROCK
-        ("Y", 2), // PAPER
-        ("Z", 3), // SCISSORS
+    let combination_points_map: HashMap<&str, i32> = HashMap::from([
+        ("A X", DRAW + ROCK),
+        ("A Y", WIN + PAPER),
+        ("A Z", LOSS + SCISSORS),
+        ("B X", LOSS + ROCK),
+        ("B Y", DRAW + PAPER),
+        ("B Z", WIN + SCISSORS),
+        ("C X", WIN + ROCK),
+        ("C Y", LOSS + PAPER),
+        ("C Z", DRAW + SCISSORS),
     ]);
 
     lines.iter().for_each(|value| {
-        let their_call = value.get(0).unwrap();
-        let your_call = value.get(1).unwrap();
-
-        let their_points = *points.get(their_call).unwrap();
-        let your_points = *points.get(your_call).unwrap();
-
-        let points_difference = your_points - their_points;
-
-        let score = your_points
-            + if points_difference == 0 {
-                DRAW
-            } else if points_difference == 1 || points_difference == -2 {
-                WIN
-            } else {
-                0
-            };
-
-        total_score += score;
+        total_score += combination_points_map.get(value).unwrap();
     });
 
     assert_eq!(total_score, 13675);
@@ -56,51 +46,27 @@ fn solve_first_part() -> i64 {
 fn solve_second_part() -> i64 {
     let lines = include_str!("./input.txt")
         .lines()
-        .map(|value| value.split(' ').collect::<Vec<&str>>())
-        .collect::<Vec<Vec<&str>>>();
+        .collect::<Vec<&str>>();
 
     let mut total_score = 0;
 
-    let points: HashMap<&str, i32> = HashMap::from([
-        ("A", 1), // ROCK
-        ("B", 2), // PAPER
-        ("C", 3), // SCISSORS
+    let combination_points_map: HashMap<&str, i32> = HashMap::from([
+        ("A X", LOSS + SCISSORS),
+        ("A Y", DRAW + ROCK),
+        ("A Z", WIN + PAPER),
+        ("B X", LOSS + ROCK),
+        ("B Y", DRAW + PAPER),
+        ("B Z", WIN + SCISSORS),
+        ("C X", LOSS + PAPER),
+        ("C Y", DRAW + SCISSORS),
+        ("C Z", WIN + ROCK),
     ]);
 
     lines.iter().for_each(|value| {
-        let their_call = value.get(0).unwrap();
-        let your_call = *value.get(1).unwrap();
-
-        let their_points = *points.get(their_call).unwrap();
-        
-        let score = if your_call == "Y" {
-            their_points + DRAW
-        } else if your_call == "Z" {
-            get_points_for_hand_beating_this_one(their_points) + WIN
-        } else {
-            get_points_for_hand_losing_to_this_one(their_points)
-        };
-
-        total_score += score;
+        total_score += combination_points_map.get(value).unwrap();
     });
 
     assert_eq!(total_score, 14184);
 
     return total_score as i64;
-}
-
-fn get_points_for_hand_beating_this_one(this_one: i32) -> i32 {
-    if this_one == 3 {
-        return 1;
-    } else {
-        return this_one + 1;
-    }
-}
-
-fn get_points_for_hand_losing_to_this_one(this_one: i32) -> i32 {
-    if this_one == 1 {
-        return 3;
-    } else {
-        return this_one - 1;
-    }
 }

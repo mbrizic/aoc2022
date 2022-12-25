c314d2d5266f272505949e735fe389d0ec032309
mbrizic <brizic.mario@gmail.com>
2022-12-25 17:39:19 +0100

    Day 11 part 2

--- src/task_11/solution.rs
+++ src/task_11/solution.rs
@@ -1,29 +1,29 @@
 use crate::common::Timer;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Monkey {
-    items: Vec<i32>,
+    items: Vec<i64>,
     operation: Operation,
-    test_divisible_by: i32,
+    test_divisible_by: i64,
     throw_if_true: usize,
     throw_if_false: usize,
-    times_inspected: i32,
+    times_inspected: i64,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Operation {
-    Add(i32),
-    Subtract(i32),
-    Multiply(i32),
+    Add(i64),
+    Subtract(i64),
+    Multiply(i64),
     Square(),
 }
 
 impl Monkey {
-    pub fn remove_first_item(&mut self) -> i32 {
+    pub fn remove_first_item(&mut self) -> i64 {
         self.items.remove(0)
     }
 
-    pub fn add_new_item(&mut self, item: i32) {
+    pub fn add_new_item(&mut self, item: i64) {
         self.items.push(item);
     }
 
@@ -35,25 +35,68 @@ impl Monkey {
 pub fn run(timer: &mut Timer) {
     let input = include_str!("./input.txt").lines().collect::<Vec<&str>>();
 
-    solve_part_1(&input, timer);
+    let monkeys = parse_monkeys(&input);
+
+    let reduction_factor = monkeys
+        .iter()
+        .fold(1, |acc, monkey| return acc * monkey.test_divisible_by);
+
+    solve_part_1(&mut monkeys.to_vec(), reduction_factor, timer);
+    solve_part_2(&mut monkeys.to_vec(), reduction_factor, timer);
 }
 
-fn solve_part_1(input: &Vec<&'static str>, timer: &mut Timer) {
-    let mut monkeys = parse_monkeys(&input);
-    let number_of_rounds = 20;
+fn solve_part_1(monkeys: &mut Vec<Monkey>, reduction_factor: i64, timer: &mut Timer) {
+    run_for_number_of_rounds(20, monkeys, reduction_factor, true);
 
-    for round_index in 0..number_of_rounds {
+    let result = extract_two_biggest_and_multiply_them(monkeys);
+
+    assert_eq!(result, 55944);
+    timer.log("11.1", result);
+}
+
+fn solve_part_2(monkeys: &mut Vec<Monkey>, reduction_factor: i64, timer: &mut Timer) {
+    run_for_number_of_rounds(10000, monkeys, reduction_factor, false);
+
+    let result = extract_two_biggest_and_multiply_them(monkeys);
+
+    assert_eq!(result, 15117269860);
+    timer.log("11.2", result);
+}
+
+fn extract_two_biggest_and_multiply_them(monkeys: &mut Vec<Monkey>) -> i64 {
+    let mut counts = monkeys
+        .iter()
+        .map(|m| m.times_inspected)
+        .collect::<Vec<i64>>();
+
+    counts.sort_unstable();
+
+    let mut two_best = counts.iter().skip(monkeys.len() - 2);
+
+    return two_best.next().unwrap() * two_best.next().unwrap();
+}
+
+fn run_for_number_of_rounds(
+    number_of_rounds: i64,
+    monkeys: &mut Vec<Monkey>,
+    reduction_factor: i64,
+    should_reduce_worry: bool,
+) {
+    for _ in 0..number_of_rounds {
         for monkey_index in 0..monkeys.len() {
             let monkey_items = &monkeys[monkey_index].items;
 
             for _ in 0..monkey_items.len() {
                 monkeys[monkey_index].increment_times_inspected();
 
-                let item_worry_level = monkeys[monkey_index].remove_first_item();
+                let mut item_worry_level = monkeys[monkey_index].remove_first_item();
 
-                let item_worry_level = solve(item_worry_level, &monkeys[monkey_index].operation);
-                // needed for first part
-                let item_worry_level = item_worry_level / 3;
+                item_worry_level =
+                    execute_operation(item_worry_level, &monkeys[monkey_index].operation);
+
+                if should_reduce_worry {
+                    item_worry_level = item_worry_level / 3;
+                }
 
                 let other_monkey_index =
                     if item_worry_level % monkeys[monkey_index].test_divisible_by == 0 {
@@ -62,41 +105,21 @@ fn solve_part_1(input: &Vec<&'static str>, timer: &mut Timer) {
                         monkeys[monkey_index].throw_if_false
                     };
 
-                // // reduce to make calculations faster
-                // let item_worry_level = item_worry_level % monkeys[other_monkey_index].test_divisible_by;
+                // Reduce numbers to make calculations faster
+                let item_worry_level = item_worry_level % reduction_factor;
 
                 monkeys[other_monkey_index].add_new_item(item_worry_level);
             }
         }
-
-        // println!("--- Round {}", round_index + 1);
-        // for monkey in &monkeys {
-        //     println!("{}", monkey.times_inspected);
-        // }
     }
-
-    let mut counts = monkeys
-        .iter()
-        .map(|m| m.times_inspected)
-        .collect::<Vec<i32>>();
-
-    counts.sort_unstable();
-
-    let mut two_best = counts.iter().skip(monkeys.len() - 2);
-
-    let result = two_best.next().unwrap() * two_best.next().unwrap();
-
-    assert_eq!(result, 55944);
-    timer.log("11.1", result);
 }
 
-fn solve(value: i32, operation: &Operation) -> i32 {
+fn execute_operation(value: i64, operation: &Operation) -> i64 {
     return match operation {
         Operation::Add(num) => value + num,
         Operation::Subtract(num) => value - num,
         Operation::Multiply(num) => value * num,
         Operation::Square() => value * value,
-        _ => panic!("Unsupported operation"),
     };
 }
 
@@ -111,8 +134,8 @@ fn parse_monkeys(input: &Vec<&'static str>) -> Vec<Monkey> {
             .next()
             .unwrap()
             .split(", ")
-            .map(|n| n.parse::<i32>().unwrap())
-            .collect::<Vec<i32>>();
+            .map(|n| n.parse::<i64>().unwrap())
+            .collect::<Vec<i64>>();
 
         let mut operation_equation = input[i + 2]
             .split("= ")
@@ -129,7 +152,7 @@ fn parse_monkeys(input: &Vec<&'static str>) -> Vec<Monkey> {
             .skip(1)
             .next()
             .unwrap()
-            .parse::<i32>()
+            .parse::<i64>()
             .unwrap();
 
         let throw_if_true = input[i + 4]
@@ -156,7 +179,7 @@ fn parse_monkeys(input: &Vec<&'static str>) -> Vec<Monkey> {
             operation_equation.next().unwrap(),
         );
 
-        let mut monkey = Monkey {
+        let monkey = Monkey {
             throw_if_false: throw_if_false,
             throw_if_true: throw_if_true,
             test_divisible_by: test,
@@ -177,17 +200,17 @@ fn parse_operation(params: &(&str, &str, &str)) -> Operation {
     }
 
     if params.1 == "*" {
-        let param = params.2.parse::<i32>().unwrap();
+        let param = params.2.parse::<i64>().unwrap();
         return Operation::Multiply(param);
     }
 
     if params.1 == "+" {
-        let param = params.2.parse::<i32>().unwrap();
+        let param = params.2.parse::<i64>().unwrap();
         return Operation::Add(param);
     }
 
     if params.1 == "-" {
-        let param = params.2.parse::<i32>().unwrap();
+        let param = params.2.parse::<i64>().unwrap();
         return Operation::Subtract(param);
     }
 
